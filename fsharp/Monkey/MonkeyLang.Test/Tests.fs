a2fc54d562fbe1b69a2b5fe181409d19762c00dd
Morgan Kenyon <morganwkenyon@gmail.com>
2019-08-22 22:18:53 -0500

    Adding unit tests and refactoring Lexer.

--- fsharp/Monkey/MonkeyLang.Test/Tests.fs
+++ fsharp/Monkey/MonkeyLang.Test/Tests.fs
@@ -3,38 +3,246 @@ module Tests
 open System
 open Xunit
 open Tokens
-open Monkey
+open Lexer
 
 let AssertTokens(lexer: LexerState, expectedToken) =
-    let actualToken = Monkey.nextToken lexer
+    let actualToken = Lexer.nextToken lexer
     Assert.Equal(expectedToken.TokenType, actualToken.TokenType)
     Assert.Equal(expectedToken.Literal, actualToken.Literal)
-    ()
+
+
+
+[<Fact>]
+let ``LookupIdent fn test`` () =
+    let fn = Lexer.lookupIdent "fn"
+    Assert.Equal(FUNCTION, fn)
+
+[<Fact>]
+let ``LookupIdent let test `` () =
+    let lt = Lexer.lookupIdent "let"
+    Assert.Equal(LET, lt)
+
+[<Fact>]
+let ``LookupIdent other test`` () =
+    let ident = Lexer.lookupIdent "ls"
+    Assert.Equal(IDENT, ident)
+    
+[<Fact>]
+let ``LookupNumber test`` () =
+    let num = Lexer.lookupNumber "2"
+    Assert.Equal(INT, num)
+    
+[<Theory>]
+[<InlineData('`', false)>]
+[<InlineData('a', true)>]
+[<InlineData('g', true)>]
+[<InlineData('z', true)>]
+[<InlineData('{', false)>]
+[<InlineData('@', false)>]
+[<InlineData('A', true)>]
+[<InlineData('M', true)>]
+[<InlineData('A', true)>]
+[<InlineData('[', false)>]
+[<InlineData('^', false)>]
+[<InlineData('_', true)>]
+[<InlineData(' ', false)>]
+let ``IsLetter correctly identifies letters`` char expectedResult =
+    Assert.Equal(expectedResult, Lexer.isLetter char)
+        
+[<Theory>]
+[<InlineData('`', false)>]
+[<InlineData('a', false)>]
+[<InlineData('g', false)>]
+[<InlineData('z', false)>]
+[<InlineData('{', false)>]
+[<InlineData('@', false)>]
+[<InlineData('A', false)>]
+[<InlineData('M', false)>]
+[<InlineData('A', false)>]
+[<InlineData('[', false)>]
+[<InlineData('^', false)>]
+[<InlineData('_', false)>]
+[<InlineData(' ', false)>]
+[<InlineData('/', false)>]
+[<InlineData('0', true)>]
+[<InlineData('5', true)>]
+[<InlineData('9', true)>]
+[<InlineData(':', false)>]
+let ``IsDigit correctly identifies digits`` char expectedResult =
+    Assert.Equal(expectedResult, Lexer.isDigit char)
+    
+[<Theory>]
+[<InlineData("aa", true)>]
+[<InlineData("a1", false)>]
+[<InlineData("a{", false)>]
+[<InlineData("aA", true)>]
+let ``CanReadLetter correctly determines when to stop reading current token`` input expected =
+    let lexer = createLexer input
+    let can = Lexer.canReadNextLetter lexer
+    Assert.Equal(expected, can)
+        
+[<Theory>]
+[<InlineData("10", true)>]
+[<InlineData("1a", false)>]
+[<InlineData("1.", false)>]
+[<InlineData("10", true)>]
+let ``CanReadDigit correctly determines when to stop reading current token`` input expected =
+    let lexer = createLexer input
+    let can = Lexer.canReadNextDigit lexer
+    Assert.Equal(expected, can)
+
+[<Theory>]
+[<InlineData("a", false)>]
+[<InlineData("aa", true)>]
+let ``CanRead doesn't error at end of input`` input expected =
+    let lexer = createLexer input
+    let can = Lexer.canReadNextLetter lexer
+    Assert.Equal(expected, can)
+
+[<Theory>]
+[<InlineData("123", '2')>]
+[<InlineData("abc", 'b')>]
+let ``readChar only reads one char at a time`` input expected =
+    let lexer = createLexer input
+    Lexer.readChar lexer |> ignore
+    Assert.Equal(expected, lexer.ch)
+
+[<Theory>]
+[<InlineData("12")>]
+[<InlineData("ab")>]
+[<InlineData("1ab")>]
+[<InlineData("sfs")>]
+let ``readChar sets ch to null when read past end of input`` input =
+    let lexer = createLexer input
+    Lexer.nextToken lexer |> ignore
+    Lexer.nextToken lexer |> ignore
+    Lexer.nextToken lexer |> ignore
+    Lexer.nextToken lexer |> ignore
+    Assert.Equal('\000', lexer.ch)
+
+[<Theory>]
+[<InlineData("5463 five", "5463")>]
+[<InlineData("546323five", "546323")>]
+let ``readNumber reads next number`` input expected =
+    let lexer = createLexer input
+    let digitToken = Lexer.readNumber lexer
+    Assert.Equal(expected, digitToken.Literal)
+    
+[<Fact>]
+let ``readIdentifier reads next let identifier`` () =
+    let lexer = createLexer "let 10"
+    let identToken = Lexer.readIdentifier lexer
+    Assert.Equal("let", identToken.Literal)
+    Assert.Equal(LET, identToken.TokenType)
+    
+[<Fact>]
+let ``readIdentifier reads next fn identifier`` () =
+    let lexer = createLexer "fn (a"
+    let identToken = Lexer.readIdentifier lexer
+    Assert.Equal("fn", identToken.Literal)
+    Assert.Equal(FUNCTION, identToken.TokenType)
+    
+[<Fact>]
+let ``readIdentifier reads next ident identifier`` () =
+    let lexer = createLexer "ten = 10"
+    let identToken = Lexer.readIdentifier lexer
+    Assert.Equal("ten", identToken.Literal)
+    Assert.Equal(IDENT, identToken.TokenType)
+    
+[<Fact>]
+let ``determineComplexTokenType identifies letter token`` () =
+    let lexer = createLexer "let 10 = z"
+    let tokenType = Lexer.determineComplexTokenType lexer
+    Assert.Equal(Letter, tokenType)
+    
+[<Fact>]
+let ``determineComplexTokenType identifies digit token`` () =
+    let lexer = createLexer "10 = z"
+    let tokenType = Lexer.determineComplexTokenType lexer
+    Assert.Equal(Digit, tokenType)
+    
+[<Fact>]
+let ``determineComplexTokenType identifies illegal token`` () =
+    let lexer = createLexer "? 10 = z"
+    let tokenType = Lexer.determineComplexTokenType lexer
+    Assert.Equal(Illegal, tokenType)
+
+[<Fact>]
+let ``nextComplexToken reads next function`` () =
+    let lexer = createLexer "fn (a"
+    let complexToken = Lexer.nextComplexToken lexer
+    Assert.Equal("fn", complexToken.Literal)
+    Assert.Equal(FUNCTION, complexToken.TokenType)
+    
+[<Fact>]
+let ``nextComplexToken reads next let identifier`` () =
+    let lexer = createLexer "let (a"
+    let identToken = Lexer.nextComplexToken lexer
+    Assert.Equal("let", identToken.Literal)
+    Assert.Equal(LET, identToken.TokenType)
+    
+[<Fact>]
+let ``nextComplexToken reads next ident identifier`` () =
+    let lexer = createLexer "ten = 10"
+    let identToken = Lexer.nextComplexToken lexer
+    Assert.Equal("ten", identToken.Literal)
+    Assert.Equal(IDENT, identToken.TokenType)
+    
+[<Fact>]
+let ``nextComplexToken reads next number`` () =
+    let lexer = createLexer "10 = 10"
+    let identToken = Lexer.nextComplexToken lexer
+    Assert.Equal("10", identToken.Literal)
+    Assert.Equal(INT, identToken.TokenType)
+    
+[<Fact>]
+let ``nextComplexToken returns illegal token`` () =
+    let lexer = createLexer "? = 10"
+    let identToken = Lexer.nextComplexToken lexer
+    Assert.Equal("?", identToken.Literal)
+    Assert.Equal(ILLEGAL, identToken.TokenType)
+
+[<Fact>]
+let ``skipWhitespaces skips whitespace`` () =
+    let lexer = createLexer "      ("
+    Lexer.skipWhitespace lexer
+    let token = Lexer.nextToken lexer
+    Assert.Equal(LPAREN, token.TokenType)
+    Assert.Equal("(", token.Literal)
+
+[<Theory>]
+[<InlineData("let")>]
+[<InlineData("10")>]
+[<InlineData("ten")>]
+[<InlineData("fn")>]
+[<InlineData("x")>]
+let ``skipWhitespace only skips whitespace`` input =
+    let lexer = createLexer input
+    Lexer.skipWhitespace lexer
+    let token = Lexer.nextToken lexer
+    Assert.Equal(input, token.Literal)
 
 [<Fact>]
 let ``Can Lex Symbols`` () =
     let expectedTokens = 
         [
-            { TokenType = TokenType.ASSIGN; Literal = "=" };
-            { TokenType = TokenType.PLUS; Literal = "+" };
-            { TokenType = TokenType.LPAREN; Literal = "(" };
-            { TokenType = TokenType.RPAREN; Literal = ")" };
-            { TokenType = TokenType.LBRACE; Literal = "{" };
-            { TokenType = TokenType.RBRACE; Literal = "}" };
-            { TokenType = TokenType.COMMA; Literal = "," };
-            { TokenType = TokenType.SEMICOLON; Literal = ";" };
-            { TokenType = TokenType.EOF; Literal = "" }
+            { TokenType = ASSIGN; Literal = "=" };
+            { TokenType = PLUS; Literal = "+" };
+            { TokenType = LPAREN; Literal = "(" };
+            { TokenType = RPAREN; Literal = ")" };
+            { TokenType = LBRACE; Literal = "{" };
+            { TokenType = RBRACE; Literal = "}" };
+            { TokenType = COMMA; Literal = "," };
+            { TokenType = SEMICOLON; Literal = ";" };
+            { TokenType = EOF; Literal = "" }
         ]
     
     let input = "=+(){},;"
-    let lexer = Monkey.createLexer input
+    let lexer = Lexer.createLexer input
     expectedTokens |> List.iter (fun et -> AssertTokens(lexer, et))
-    ()
 
 [<Fact>]
 let ``Can Lex Simple Program`` () =
-// [<Fact>]
- //let ``Can Lex Simple Program`` () =
     let input = 
         "let five = 5;
 let ten = 10;
@@ -45,54 +253,59 @@ let add = fn(x, y) {
 
 let result = add(five, ten);"  
     let expectedTokens = [
-        { TokenType = TokenType.LET; Literal = "let" };
-        { TokenType = TokenType.IDENT; Literal = "five" };
-        { TokenType = TokenType.ASSIGN; Literal = "=" };
-        { TokenType = TokenType.INT; Literal = "5" };
-        { TokenType = TokenType.SEMICOLON; Literal = ";" };
+        { TokenType = LET; Literal = "let" };
+        { TokenType = IDENT; Literal = "five" };
+        { TokenType = ASSIGN; Literal = "=" };
+        { TokenType = INT; Literal = "5" };
+        { TokenType = SEMICOLON; Literal = ";" };
 
         
-        { TokenType = TokenType.LET; Literal = "let" };
-        { TokenType = TokenType.IDENT; Literal = "ten" };
-        { TokenType = TokenType.ASSIGN; Literal = "=" };
-        { TokenType = TokenType.INT; Literal = "10" };
-        { TokenType = TokenType.SEMICOLON; Literal = ";" };
+        { TokenType = LET; Literal = "let" };
+        { TokenType = IDENT; Literal = "ten" };
+        { TokenType = ASSIGN; Literal = "=" };
+        { TokenType = INT; Literal = "10" };
+        { TokenType = SEMICOLON; Literal = ";" };
 
         
-        { TokenType = TokenType.LET; Literal = "let" };
-        { TokenType = TokenType.IDENT; Literal = "add" };
-        { TokenType = TokenType.ASSIGN; Literal = "=" };
-        { TokenType = TokenType.FUNCTION; Literal = "fn" };
-        { TokenType = TokenType.LPAREN; Literal = "(" };
-        { TokenType = TokenType.IDENT; Literal = "x" };
-        { TokenType = TokenType.COMMA; Literal = "," };
-        { TokenType = TokenType.IDENT; Literal = "y" };
-        { TokenType = TokenType.RPAREN; Literal = ")" };
-        { TokenType = TokenType.LBRACE; Literal = "{" };
-        { TokenType = TokenType.IDENT; Literal = "x" };
-        { TokenType = TokenType.PLUS; Literal = "+" };
-        { TokenType = TokenType.IDENT; Literal = "y" };
-        { TokenType = TokenType.SEMICOLON; Literal = ";" };
-        { TokenType = TokenType.RBRACE; Literal = "}" };
-        { TokenType = TokenType.SEMICOLON; Literal = ";" };
+        { TokenType = LET; Literal = "let" };
+        { TokenType = IDENT; Literal = "add" };
+        { TokenType = ASSIGN; Literal = "=" };
+        { TokenType = FUNCTION; Literal = "fn" };
+        { TokenType = LPAREN; Literal = "(" };
+        { TokenType = IDENT; Literal = "x" };
+        { TokenType = COMMA; Literal = "," };
+        { TokenType = IDENT; Literal = "y" };
+        { TokenType = RPAREN; Literal = ")" };
+        { TokenType = LBRACE; Literal = "{" };
+        { TokenType = IDENT; Literal = "x" };
+        { TokenType = PLUS; Literal = "+" };
+        { TokenType = IDENT; Literal = "y" };
+        { TokenType = SEMICOLON; Literal = ";" };
+        { TokenType = RBRACE; Literal = "}" };
+        { TokenType = SEMICOLON; Literal = ";" };
 
         
-        { TokenType = TokenType.LET; Literal = "let" };
-        { TokenType = TokenType.IDENT; Literal = "result" };
-        { TokenType = TokenType.ASSIGN; Literal = "=" };
-        { TokenType = TokenType.IDENT; Literal = "add" };
-        { TokenType = TokenType.LPAREN; Literal = "(" };
-        { TokenType = TokenType.IDENT; Literal = "five" };
-        { TokenType = TokenType.COMMA; Literal = "," };
-        { TokenType = TokenType.IDENT; Literal = "ten" };
-        { TokenType = TokenType.RPAREN; Literal = ")" };
-        { TokenType = TokenType.SEMICOLON; Literal = ";" };
-        { TokenType = TokenType.EOF; Literal = "" };    
+        { TokenType = LET; Literal = "let" };
+        { TokenType = IDENT; Literal = "result" };
+        { TokenType = ASSIGN; Literal = "=" };
+        { TokenType = IDENT; Literal = "add" };
+        { TokenType = LPAREN; Literal = "(" };
+        { TokenType = IDENT; Literal = "five" };
+        { TokenType = COMMA; Literal = "," };
+        { TokenType = IDENT; Literal = "ten" };
+        { TokenType = RPAREN; Literal = ")" };
+        { TokenType = SEMICOLON; Literal = ";" };
+        { TokenType = EOF; Literal = "" };    
     ]
     
-    let lexer = Monkey.createLexer input
-    for expectedToken in expectedTokens do
-        let token = Monkey.nextToken lexer
-        Assert.Equal(expectedToken.TokenType, token.TokenType)
-        Assert.Equal(expectedToken.Literal, token.Literal)
-    ()
+    let lexer = Lexer.createLexer input
+    expectedTokens |> List.iter (fun et -> AssertTokens(lexer, et))
+
+[<Fact>]
+let ``CreateLexer initializes lexer correctly``() =
+    let input = "(){}"
+    let lexer = Lexer.createLexer input
+    Assert.Equal("(){}", lexer.input)
+    Assert.Equal(0, lexer.position)
+    Assert.Equal(1, lexer.readPosition)
+    Assert.Equal('(', lexer.ch)
---
+++ fsharp/Monkey/MonkeyLang/Lexer.fs
@@ -0,0 +1,134 @@
+﻿module Lexer
+
+
+open Tokens
+
+type LexerState (input: string)=
+    let _input = input
+    let mutable _position = 0
+    let mutable _readPosition = 0
+    let mutable _ch = '\000'
+    member l.input
+        with get() = _input
+    member l.position 
+        with get() = _position
+        and internal set v =
+            _position <- v
+    member l.readPosition
+        with get() = _readPosition
+        and internal set v =
+            _readPosition <- v
+    member l.ch
+        with get() = _ch
+        and internal set v =
+            _ch <- v
+
+
+type ComplexTokenType =
+    | Letter
+    | Digit
+    | Illegal
+
+/// Determines the TokenType of the string token
+let internal lookupIdent ident =
+    if ident = "fn" then
+        FUNCTION
+    else if ident = "let" then
+        LET
+    else
+        IDENT
+
+/// Determines the TokenType of the number token
+let internal lookupNumber number = INT
+
+/// Determines whether the char is a letter
+let internal isLetter(ch: char) =
+    let lowerCase = ch.CompareTo('a') >= 0 && ch.CompareTo('z') <= 0
+    let upperCase = ch.CompareTo('A') >= 0 && ch.CompareTo('Z') <= 0;
+    let underscore = ('_' = ch);
+    lowerCase || upperCase || underscore
+
+/// Determines whether the char is a digit    
+let internal isDigit(ch: char) =
+    ch.CompareTo('0') >= 0 && ch.CompareTo('9') <= 0
+
+/// Determines whether next char is part of current token
+let private canReadNext(can:char -> bool) (l: LexerState) =
+    match (l.position + 1) < l.input.Length with
+    | true -> can(l.input.Chars(l.position + 1))
+    | false -> false
+
+/// Determines whether next char is part of current digit token
+let internal canReadNextDigit(l: LexerState) = canReadNext isDigit l
+
+/// Determines whether next char is part of current letter token
+let internal canReadNextLetter(l: LexerState) = canReadNext isLetter l
+
+/// Advances state to the next char in the input
+let internal readChar (l: LexerState) =
+    let newChar = 
+        match l.readPosition >= l.input.Length with
+        | true -> '\000'
+        | false -> l.input.Chars l.readPosition
+    l.ch <- newChar
+    l.position <- l.readPosition
+    l.readPosition <- l.readPosition + 1
+
+/// Calculates the token
+let private readChunk (canRead: LexerState -> bool) (lookupToken: string -> TokenType) (l: LexerState) =
+    let pos = l.position
+    while canRead(l) do
+        readChar l
+    let literal = l.input.Substring(pos, (l.position - pos + 1))
+    let tokenType = lookupToken literal
+    {TokenType = tokenType; Literal = literal}
+
+/// Calculates the next number token
+let internal readNumber(l: LexerState) = readChunk canReadNextDigit lookupNumber l
+
+/// Calculates the next string token
+let internal readIdentifier(l: LexerState) = readChunk canReadNextLetter lookupIdent l
+
+/// Determines whether the next token is part of a valid ComplexTokenType or not
+let internal determineComplexTokenType (l: LexerState) =
+    if isLetter(l.ch) then Letter
+    else if isDigit(l.ch) then Digit
+    else Illegal
+
+/// Calculates the next complex token
+let internal nextComplexToken(l: LexerState) =
+    match determineComplexTokenType(l) with 
+    | Letter -> readIdentifier(l)
+    | Digit -> readNumber(l)
+    | Illegal -> {TokenType = TokenType.ILLEGAL; Literal = l.ch.ToString()}
+    
+/// Skips any whitespace encountered in the input
+let internal skipWhitespace(l: LexerState) =
+    while l.ch = ' ' || l.ch = '\t' || l.ch = '\n' || l.ch = '\r' do
+        readChar l
+    
+/// Reads the next available token in the input
+let nextToken (l: LexerState) =
+
+    skipWhitespace l
+
+    let nextToken = 
+        match l.ch with
+        | '=' ->    { TokenType = TokenType.ASSIGN; Literal = l.ch.ToString()}
+        | '+' ->    { TokenType = TokenType.PLUS; Literal = l.ch.ToString()}
+        | ';' ->    { TokenType = TokenType.SEMICOLON; Literal = l.ch.ToString()}
+        | '(' ->    { TokenType = TokenType.LPAREN; Literal = l.ch.ToString()}
+        | ')' ->    { TokenType = TokenType.RPAREN; Literal = l.ch.ToString()}
+        | '{' ->    { TokenType = TokenType.LBRACE; Literal = l.ch.ToString()}
+        | '}' ->    { TokenType = TokenType.RBRACE; Literal = l.ch.ToString()}
+        | ',' ->    { TokenType = TokenType.COMMA; Literal = l.ch.ToString()}
+        | '\000' -> { TokenType = TokenType.EOF; Literal = ""}
+        | _ ->      nextComplexToken(l)
+    readChar l
+    nextToken
+    
+/// Creates the Lexer
+let createLexer input =
+    let lexer = LexerState(input)
+    readChar lexer
+    lexer
