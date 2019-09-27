0efe447a6add6869a97a0ed6e371437236c1f15d
Morgan Kenyon <morganwkenyon@gmail.com>
2019-09-27 06:09:30 -0500

    Continue starting work on parsing.

---
+++ newsrc/MonkeyLang/AST/Parser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MonkeyLang.Lexical;
+
+namespace MonkeyLang.AST
+{
+    public class Parser
+    {
+        public Lexer Lexer { get; private set; }
+        public Token curToken { get; private set; }
+        public Token peekToken { get; private set; }
+
+        public Parser(Lexer lexer)
+        {
+            Lexer = lexer;
+
+            NextToken();
+            NextToken();
+        }
+
+        public void NextToken()
+        {
+            curToken = peekToken;
+            peekToken = Lexer.NextToken();
+        }
+
+        public Program ParseProgram()
+        {
+            return null;
+        }
+    }
+}
