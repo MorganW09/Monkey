aa112e58adaaf298477294dc7c81db4fe056cebb
Morgan Kenyon <morganwkenyon@gmail.com>
2023-01-27 20:22:45 -0600

    Adding more advanced tokenization functionality

--- src/MonkeyLang.Lib/Lexer.fs
+++ src/MonkeyLang.Lib/Lexer.fs
@@ -8,6 +8,19 @@ module Lexer
             mutable ch : char
         }
 
+    type ComplexTokenType =
+        | Letter
+        | Digit
+        | Illegal
+
+    let lookupIdent ident =
+        if ident = "fn" then
+            FUNCTION
+        else if ident = "let" then
+            LET
+        else
+            IDENT
+
     let readChar (l: LexerState) =
         let newChar =
             match l.readPosition >= l.input.Length with
@@ -17,23 +30,71 @@ module Lexer
         l.readPosition <- l.readPosition + 1
         l.ch <- newChar
 
+    let isLetter(ch: char) =
+        let lowerCase = ch.CompareTo('a') >= 0 && ch.CompareTo('z') <= 0
+        let upperCase = ch.CompareTo('A') >= 0 && ch.CompareTo('Z') <= 0;
+        let underscore = ('_' = ch);
+        lowerCase || upperCase || underscore
+
+    let canReadLetter(l: LexerState) =
+        isLetter(l.input.Chars(l.position + 1))
+
+    let readIdentifier(l: LexerState) =
+        let pos = l.position
+        while canReadLetter(l) do
+            readChar l
+        let literal = l.input.Substring(pos, (l.position - pos + 1))
+        let tokenType = lookupIdent literal
+        (tokenType, literal)
+
+    let isDigit(ch: char) =
+        ch.CompareTo('0') >= 0 && ch.CompareTo('9') <= 0
+
+    let canReadDigit(l: LexerState) =
+        isDigit(l.input.Chars(l.position + 1))
+
+    let readNumber(l: LexerState) =
+        let pos = l.position
+        while canReadDigit(l) do 
+            readChar l
+        let literal = l.input.Substring(pos, (l.position - pos + 1))
+        (INT, literal)
+    
+    let findComplexTokenType l =
+        if isLetter(l.ch) then
+            Letter
+        else if isDigit(l.ch) then
+            Digit
+        else
+            Illegal
+
+    let nextComplexToken(l: LexerState) =
+        match findComplexTokenType(l) with 
+        | Letter -> readIdentifier(l)
+        | Digit -> readNumber(l)
+        | Illegal -> (TokenType.ILLEGAL, l.ch.ToString())
+
+    let skipWhitespace(l: LexerState) =
+        while l.ch = ' ' || l.ch = '\t' || l.ch = '\n' || l.ch = '\r' do
+            readChar l
+        ()
+
     let nextToken (l: LexerState) =
-        let tokenType =
+
+        skipWhitespace l
+
+        let (tokenType, literal) =
             match l.ch with
-            | '=' -> TokenType.ASSIGN
-            | ';' -> TokenType.SEMICOLON
-            | '(' -> TokenType.LPAREN
-            | ')' -> TokenType.RPAREN
-            | ',' -> TokenType.COMMA
-            | '+' -> TokenType.PLUS
-            | '{' -> TokenType.LBRACE
-            | '}' -> TokenType.RBRACE
-            | _ -> TokenType.EOF
-        
-        let literal =
-            match tokenType with
-            | TokenType.EOF -> ""
-            | _ -> l.ch.ToString()
+            | '=' -> (TokenType.ASSIGN, l.ch.ToString())
+            | ';' -> (TokenType.SEMICOLON, l.ch.ToString())
+            | '(' -> (TokenType.LPAREN, l.ch.ToString())
+            | ')' -> (TokenType.RPAREN, l.ch.ToString())
+            | ',' -> (TokenType.COMMA, l.ch.ToString())
+            | '+' -> (TokenType.PLUS, l.ch.ToString())
+            | '{' -> (TokenType.LBRACE, l.ch.ToString())
+            | '}' -> (TokenType.RBRACE, l.ch.ToString())
+            | '\000' -> (TokenType.EOF, "")
+            | _ -> nextComplexToken l
 
         let token = { TokenType = tokenType; Literal = literal }
         
--- src/MonkeyLang.Tests/LexerTests.fs
+++ src/MonkeyLang.Tests/LexerTests.fs
@@ -11,8 +11,11 @@ let AssertTokens(lexer: LexerState, expectedToken) =
     Assert.Equal(expectedToken.Literal, actualToken.Literal)
     ()
 
+let buildTokenTypes(tokens) =
+    tokens |> List.map (fun (t, l) -> { TokenType = t ; Literal = l })
+
 [<Fact>]
-let ``Can Lex Symbols`` () =
+let ``Can Lex Basic Symbols`` () =
     let expectedTokens =
         [
             { TokenType = TokenType.ASSIGN ; Literal = "=" };
@@ -27,5 +30,59 @@ let ``Can Lex Symbols`` () =
         ]
     
     let input = "=+(){},;"
+    let lexer = createLexer input
+    expectedTokens |> List.iter (fun et -> AssertTokens(lexer, et))
+
+[<Fact>]
+let ``Can Lex More Advanced Tokens`` () =
+    let input = "let five = 5;
+let ten = 10;
+let add = fn(x, y) {
+x + y;
+};
+let result = add(five, ten);"
+    
+    let expectedTokensRaw:(TokenType * string) list =
+        [
+            (TokenType.LET, "let");
+            (TokenType.IDENT, "five");
+            (TokenType.ASSIGN, "=");
+            (TokenType.INT, "5");
+            (TokenType.SEMICOLON, ";");
+            (TokenType.LET, "let");
+            (TokenType.IDENT, "ten");
+            (TokenType.ASSIGN, "=");
+            (TokenType.INT, "10");
+            (TokenType.SEMICOLON, ";");
+            (TokenType.LET, "let");
+            (TokenType.IDENT, "add");
+            (TokenType.ASSIGN, "=");
+            (TokenType.FUNCTION, "fn");
+            (TokenType.LPAREN, "(");
+            (TokenType.IDENT, "x");
+            (TokenType.COMMA, ",");
+            (TokenType.IDENT, "y");
+            (TokenType.RPAREN, ")");
+            (TokenType.LBRACE, "{");
+            (TokenType.IDENT, "x");
+            (TokenType.PLUS, "+");
+            (TokenType.IDENT, "y");
+            (TokenType.SEMICOLON, ";");
+            (TokenType.RBRACE, "}");
+            (TokenType.SEMICOLON, ";");
+            (TokenType.LET, "let");
+            (TokenType.IDENT, "result");
+            (TokenType.ASSIGN, "=");
+            (TokenType.IDENT, "add");
+            (TokenType.LPAREN, "(");
+            (TokenType.IDENT, "five");
+            (TokenType.COMMA, ",");
+            (TokenType.IDENT, "ten");
+            (TokenType.RPAREN, ")");
+            (TokenType.SEMICOLON, ";");
+            (TokenType.EOF, "");
+        ]
+
+    let expectedTokens = buildTokenTypes expectedTokensRaw
     let lexer = createLexer input
     expectedTokens |> List.iter (fun et -> AssertTokens(lexer, et))
