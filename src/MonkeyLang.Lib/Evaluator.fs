052a95b8207b804c4e7943a3ade4727a2bfbe48d
Morgan Kenyon <morganwkenyon@gmail.com>
2023-06-26 10:45:54 -0500

    Adding evaluation for infix expressions and if else statements

--- src/MonkeyLang.Lib/Evaluator.fs
+++ src/MonkeyLang.Lib/Evaluator.fs
@@ -1,5 +1,106 @@
 module Evaluator
 
+    
+
+    let trueBooleanObject = new Object.Boolean(true)
+    let falseBooleanObject = new Object.Boolean(false)
+    let nullObject = new Object.Null()
+
+    let toObj obj = obj :> Object.Object
+
+    let isTruthy (obj : Object.Object)  =
+        match obj.Type() with
+        | Object.NULL -> false
+        | Object.BOOLEAN ->
+            let bool = obj :?> Object.Boolean
+            bool.value
+        | _ -> true
+
+    let nativeBoolToBooleanObject input =
+        if input then trueBooleanObject
+        else falseBooleanObject
+
+    let evalBangOperatorExpression (right: Object.Object) =
+        match right.Type() with
+        | Object.ObjectType.BOOLEAN ->
+            let bool = right :?> Object.Boolean
+
+            nativeBoolToBooleanObject (not bool.value)
+        | Object.ObjectType.NULL -> trueBooleanObject
+        | _ -> falseBooleanObject
+
+    let evalMinusPrefixOperatorExpression (right: Object.Object) =
+        match right.Type() with
+        | Object.ObjectType.INTEGER ->
+            let int = right :?> Object.Integer
+
+            let reversedInt = int.value * -1L
+
+            let reversedObj = new Object.Integer(reversedInt)
+
+            (reversedObj :> Object.Object)
+        | _ -> (nullObject :> Object.Object)
+
+    let evalIntegerInfixExpression operator (left: Object.Integer) (right: Object.Integer) =
+        match operator with
+        | "+" -> 
+            new Object.Integer(left.value + right.value)
+            |> toObj
+        | "-" -> 
+            new Object.Integer(left.value - right.value)
+            |> toObj
+        | "*" -> 
+            new Object.Integer(left.value * right.value)
+            |> toObj
+        | "/" -> 
+            new Object.Integer(left.value / right.value)
+            |> toObj
+        | "<" ->
+            nativeBoolToBooleanObject(left.value < right.value)
+            |> toObj
+        | ">" ->
+            nativeBoolToBooleanObject(left.value > right.value)
+            |> toObj
+        | "==" ->
+            nativeBoolToBooleanObject(left.value = right.value)
+            |> toObj
+        | "!=" -> 
+            nativeBoolToBooleanObject(left.value <> right.value)
+            |> toObj
+        | _ -> toObj nullObject
+
+    let evalBooleanInfixExpression operator (left: Object.Boolean) (right: Object.Boolean) =
+        match operator with
+        | "==" ->
+            new Object.Boolean(left.value = right.value)
+            |> toObj
+        | "!=" -> 
+            new Object.Boolean(left.value <> right.value)
+            |> toObj
+        | _ -> toObj nullObject
+
+
+    let evalPrefixExpression operator right =
+        match operator with
+        | "!" ->
+            let booleanObject = evalBangOperatorExpression right
+            booleanObject :> Object.Object
+        | "-" ->
+            evalMinusPrefixOperatorExpression right
+        | _ -> nullObject :> Object.Object
+
+    let evalInfixExpression operator (left: Object.Object) (right: Object.Object) =
+        match left.Type(), right.Type() with
+        | Object.ObjectType.INTEGER, Object.ObjectType.INTEGER ->
+            let leftInt = left :?> Object.Integer
+            let rightInt = right :?> Object.Integer
+            evalIntegerInfixExpression operator leftInt rightInt
+        | Object.ObjectType.BOOLEAN, Object.ObjectType.BOOLEAN ->
+            let leftBool = left :?> Object.Boolean
+            let rightBool = right :?> Object.Boolean
+            evalBooleanInfixExpression operator leftBool rightBool
+        | _ -> nullObject :> Object.Object
+
     let rec eval (node: Ast.Node) =
         match node.AType() with 
         | Ast.Program -> 
@@ -14,8 +115,36 @@ module Evaluator
             Some (int :> Object.Object)
         | Ast.Boolean ->
             let boolLit = node :?> Ast.Boolean
-            let boolObj = new Object.Boolean(boolLit.value)
+            let boolObj = 
+                if boolLit.value then trueBooleanObject
+                else falseBooleanObject
             Some (boolObj :> Object.Object)
+        | Ast.PrefixExpression ->
+            let preExpr = node :?> Ast.PrefixExpression
+            let right = eval preExpr.right
+
+            match right with
+            | Some r ->
+                let result = evalPrefixExpression preExpr.operator r
+                Some result
+            | None -> None
+        | Ast.InfixExpression ->
+            let inExpr = node :?> Ast.InfixExpression
+
+            let left = eval inExpr.left
+            let right = eval inExpr.right
+
+            match left, right with
+            | Some l, Some r ->
+                let result = evalInfixExpression inExpr.operator l r
+                Some result
+            | _, _ -> None
+        | Ast.BlockStatement ->
+            let block = node :?> Ast.BlockStatement
+            evalStatements block.statements
+        | Ast.IfExpression ->
+            let ifExpr = node :?> Ast.IfExpression
+            evalIfExpression ifExpr
         | _ -> None
 
     and evalStatements (stmts: Ast.Statement[]) =
@@ -25,3 +154,14 @@ module Evaluator
             result <- eval stmt
 
         result
+    and evalIfExpression (ifExpr: Ast.IfExpression) =
+        let condition = eval ifExpr.condition
+
+        match condition with
+        | Some c ->
+            if isTruthy c then
+                eval ifExpr.consequence
+            else if ifExpr.alternative.IsSome then
+                eval ifExpr.alternative.Value
+            else Some (nullObject |> toObj)
+        | None -> Some (nullObject |> toObj)
--- src/MonkeyLang.Tests/EvaluatorTests.fs
+++ src/MonkeyLang.Tests/EvaluatorTests.fs
@@ -3,7 +3,7 @@ module EvaluatorTests
 open Xunit
 open Lexer
 open Parser
-open Tokens
+open System
 
 let canDowncastToInteger (s: Object.Object) =
     match s with 
@@ -12,6 +12,10 @@ let canDowncastToInteger (s: Object.Object) =
 let canDowncastToBoolean (s: Object.Object) =
     match s with 
     | :? Object.Boolean as int -> true
+    | _ -> false
+let canDowncastToNull (s: Object.Object) =
+    match s with 
+    | :? Object.Null as int -> true
     | _ -> false
 
 let testEval input =
@@ -45,9 +49,29 @@ let testBooleanObject (obj: Object.Object option) expected =
 
     Assert.True((expected = bool.value), msg)
 
+let testNullObject (obj: Object.Object option) =
+    Assert.True(obj.IsSome, "IsNone")
+
+    let someObj = obj.Value
+    Assert.True(canDowncastToNull someObj)
+
 [<Theory>]
 [<InlineData("5", 5L)>]
 [<InlineData("10", 10L)>]
+[<InlineData("-5", -5L)>]
+[<InlineData("-10", -10L)>]
+[<InlineData("5 + 5 + 5 + 5 - 10", 10)>]
+[<InlineData("2 * 2 * 2 * 2 * 2", 32)>]
+[<InlineData("-50 + 100 + -50", 0)>]
+[<InlineData("5 * 2 + 10", 20)>]
+[<InlineData("5 + 2 * 10", 25)>]
+[<InlineData("20 + 2 * -10", 0)>]
+[<InlineData("50 / 2 * 2 + 10", 60)>]
+[<InlineData("2 * (5 + 10)", 30)>]
+[<InlineData("3 * 3 * 3 + 10", 37)>]
+[<InlineData("3 * (3 * 3) + 10", 37)>]
+[<InlineData("(5 + 10 * 2 + 15 / 3) * 2 + -10", 50)>]
+
 let ``Can Test and Eval Integers`` input expected =
 
     let evaluated = testEval input
@@ -58,10 +82,54 @@ let ``Can Test and Eval Integers`` input expected =
 [<Theory>]
 [<InlineData("true", true)>]
 [<InlineData("false", false)>]
+[<InlineData("1 < 2", true)>]
+[<InlineData("1 > 2", false)>]
+[<InlineData("1 < 1", false)>]
+[<InlineData("1 > 1", false)>]
+[<InlineData("1 == 1", true)>]
+[<InlineData("1 != 1", false)>]
+[<InlineData("1 == 2", false)>]
+[<InlineData("1 != 2", true)>]
+[<InlineData("true == true", true)>]
+[<InlineData("false == false", true)>]
+[<InlineData("true == false", false)>]
+[<InlineData("true != false", true)>]
+[<InlineData("false != true", true)>]
+[<InlineData("(1 < 2) == true", true)>]
+[<InlineData("(1 < 2) == false", false)>]
+[<InlineData("(1 > 2) == true", false)>]
+[<InlineData("(1 > 2) == false", true)>]
 let ``Can Test and Eval Booleans`` input expected =
 
     let evaluated = testEval input
 
     testBooleanObject evaluated expected
     
-//117
+[<Theory>]
+[<InlineData("!true", false)>]
+[<InlineData("!false", true)>]
+[<InlineData("!5", false)>]
+[<InlineData("!!true", true)>]
+[<InlineData("!!false", false)>]
+[<InlineData("!!5", true)>]
+let ``Can test bang operator`` input expected =
+    let evaluated = testEval input
+
+    testBooleanObject evaluated expected
+
+[<Theory>]
+[<InlineData("if (true) { 10 }", 10L)>]
+[<InlineData("if (false) { 10 }", null)>]
+[<InlineData("if (1) { 10 }", 10L)>]
+[<InlineData("if (1 < 2) { 10 }", 10L)>]
+[<InlineData("if (1 > 2) { 10 }", null)>]
+[<InlineData("if (1 > 2) { 10 } else { 20 }", 20L)>]
+[<InlineData("if (1 < 2) { 10 } else { 20 }", 10L)>]
+let ``Can test if else expressions`` input (expected: int64 Nullable ) =
+    let evaluated = testEval input
+
+    match expected.HasValue with 
+    | true ->
+        testIntegerObject evaluated expected.Value
+    | false -> 
+        testNullObject evaluated
